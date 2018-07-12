d8c1102b49dead145df5d15c0895b4f73fd41363
Dylan Jones <dylanjones2011@gmail.com>
2018-07-12 15:10:18 -0400

    this instanceof Runnable()

--- core/__init__.py
+++ core/__init__.py
@@ -6,57 +6,57 @@ from . import mode
 from . import power
 
 
-logger = logging.getLogger()
-
-
 with open('config.yml') as f:
     config = yaml.load(f)
 
 
 def enter_normal_mode(reason: str='') -> None:
     global current_mode
-    logger.info(f"Entering normal mode.{'  Reason: ' if reason else ''}{reason}")
+    logging.info(f"Entering normal mode.{'  Reason: ' if reason else ''}{reason}")
     current_mode = mode.NORMAL
     # Trigger the module hooks
     for module in submodules:
-        getattr(module, 'enter_normal_mode')()
+        if hasattr(module, 'enter_normal_mode'):
+            getattr(module, 'enter_normal_mode')()
 
 
 def enter_low_power_mode(reason: str='') -> None:
     global current_mode
-    logger.info(f"Entering low_power mode.{'  Reason: ' if reason else ''}{reason}")
+    logging.info(f"Entering low_power mode.{'  Reason: ' if reason else ''}{reason}")
     current_mode = mode.LOW_POWER
     # Trigger the module hooks
     for module in submodules:
-        getattr(module, 'enter_low_power_mode')()
+        if hasattr(module, 'enter_low_power_mode'):
+            getattr(module, 'enter_low_power_mode')()
 
 
 def enter_emergency_mode(reason: str='') -> None:
     global current_mode
-    logger.info(f"Entering emergency mode.{'  Reason: ' if reason else ''}{reason}")
+    logging.info(f"Entering emergency mode.{'  Reason: ' if reason else ''}{reason}")
     current_mode = mode.EMERGENCY
     # Trigger the module hooks
     for module in submodules:
-        getattr(module, 'enter_emergency_mode')()
+        if hasattr(module, 'enter_emergency_mode'):
+            getattr(module, 'enter_emergency_mode')()
 
 
 def startup():
     global submodules
-    logger.debug("Config:")
-    logger.debug(config)
+    logging.debug("Config:")
+    logging.debug(config)
     # Load all the modules
     submodules = []
     for module in config['modules']:
-        logger.debug(f'Loading module {module}')
+        logging.debug(f'Loading module {module}')
         submodules.append(importlib.import_module(f'submodules.{module}'))
     # Trigger module startup
     for module in submodules:
-        getattr(module, 'on_startup')()
-
+        if hasattr(module, 'on_startup'):
+            getattr(module, 'on_startup')()
+    logging.debug("Entering main loop.")
     # Main loop?
     while True:
-        sleep(1)
-
+        time.sleep(1)
 
 
 current_mode = mode.NORMAL
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import sys
 logger = logging.getLogger()
 if '--debug' in sys.argv or '-d' in sys.argv:
     logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
 
 if __name__ == '__main__':
+    logging.info("Starting application")
     core.startup()
