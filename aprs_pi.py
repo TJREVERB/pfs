a919e75c0676b960e1f8590104333c595f326a71
Shasty88 <shihaocao@gmail.com>
2018-07-26 21:01:34 -0400

    shihao comments things on aprs_pi.py

--- aprs_pi.py
+++ aprs_pi.py
@@ -1,3 +1,8 @@
+#YOU NEED TO RUN pip install pyserial and pip install logging
+#TO HAVE ACCESS TO THE BELOW MODULES. DO THAT IN POWERSHELL OR CMDER OR BASH
+#CONTACT SHIHAO OR DYLAN IF YOU HAVE questions
+#DO NOT MODIFY THIS FILE, main.py or config.yaml WITHOUT MY PERMISSION
+
 import serial
 import time
 import sys
@@ -7,65 +12,92 @@ import logging
 #from submodules.command_ingest import piprint
 #import command_ingest as ci
 #from submodules import command_ingest as ci
-from submodules import *
 
+#THIS IMPORTS ALL SUBMODULES
+from submodules import *
+#IMPORT THREADING
 from threading import Thread
 #from core import config
+
+#THIS IS FOR APRS ONLY:
 user = False
 if len(sys.argv) > 1:
     if sys.argv[1] == 'user':
         user = True
 
-#open port
+#THIS WILL SEND A MESSAGE TO THE DEVICE
+#IT IS EQUIVALENT TO TYPING IN PUTTY
 def send(msg):
+    #THIS LINE IS NEEDED
+    #IT IS THE EQUIVALENT OF PRESSING ENTER IN PUTTY
     msg = msg + "\r\n"
     #logging.debug("Hidylan")
     #print(msg)
     #print(bytes(msg,encoding="utf-8"))
     #ser.write(bytes(msg,encoding="utf-8"))
+    #TURNS YOUR STRING INTO BYTES
+    #NEEDED TO PROPERLY SEND OVER SERIAL
     ser.write(msg.encode("utf-8"))
+#THIS METHOD THREAD RUNS FOREVER ONCE STARTED
+#AND PRINTS ANYTHING IT RECIEVES OVER THE SERIAL LINE
 def listen():
     while(True):
+        #IF I GET SOMETHING OVER THE SERIAL LINE
         zz = ser.inWaiting()
+        #READ THAT MANY BYTES
         rr = ser.read(size = zz)
         if zz > 0:
             time.sleep(.5)
+            #CHECK AFTER .5 SECONDS, AND READ ANYTHING THAT GOT LEFT BEHIND
             zz = ser.inWaiting()
             rr += ser.read(size = zz)
             print(rr)
             #return (rr)
             #return rr
 
-
 def keyin():
     while(True):
+        #GET INPUT FROM YOUR OWN TERMINAL
+        #TRY input("shihaoiscoolforcommentingstuff") IF raw_input() doesn't work
         in1 = raw_input("Type Command: ")
         if(user):
             send("TJ" + in1 + chr(sum([ord(x) for x in "TJ"+in1])%128))
         else:
             send(in1)
+        #FOR ANY NON APRS MODULES THE ABOVE "IF" LOGIC IS UN-NEEDED.
+        #JUST USE SEND
 
+#APRS ONLY
 def beacon():
     while(True):
         time.sleep(bperiod)
         btext = "HELLO WORLD I AM FROMETH TJ"
         send(btext)
 
-
+#ANYTHING IN HERE WILL BE EXECUTED ON STARTUP
 def on_startup():
+    #GLOBAL VARIABLES ARE NEEDED IF YOU "CREATE" VARIABLES WITHIN THIS METHOD
+    #AND ACCESS THEM ELSEWHERE
     global bperiod, t1, ser
     bperiod = 60
     #serialPort = config['aprs']['serial_port']
+    #REPLACE WITH COMx IF ON WINDOWS
+    #REPLACE WITH /dev/ttyUSBx if 1 DOESNT WORK
     serialPort = "/dev/ttyUSB0"
+    #OPENS THE SERIAL PORT FOR ALL METHODS TO USE WITH 19200 BAUD
     ser = serial.Serial(serialPort, 19200)
+    #CREATES A THREAD THAT RUNS THE LISTEN METHOD
     t1 = Thread(target=listen, args=())
     t1.daemon = True
     t1.start()
     #logging.debug("Test")
+
+#HAVE THE 3 BELOW METHODS. SAY PASS IF YOU DONT KNOW WHAT TO PUT THERE YET
+#THESE ARE IN REFERENCE TO POWER LEVELS. SHUT STUFF DOWN IF WE NEED TO GO TO
+#EMERGYENCY MODE OR LOW POWER. ENTERING NORMAL MODE SHOULD TURN THEM BACK ON
 def enter_normal_mode():
     global bperiod
     bperiod = 60
-
 
 def enter_low_power_mode():
     global bperiod
@@ -74,13 +106,17 @@ def enter_low_power_mode():
 def enter_emergency_mode():
     pass
 
-
+#ANYTHING IN HERE WILL EXECUTE IF YOU RUN python aprs_pi.py
+#IT IS THE SAME AS MAIN IN JAVA
 if __name__ == '__main__':
+    #CALLS THE STUFF TO HAPPEN ON STARTUP
     on_startup()
     #serialPort = sys.argv[1]
     #ser = serial.Serial(serialPort, 19200)
+    #THIS STARTS YOUR THREAD TO LISTEN FOR KEYBOARD INPUT
     t2 = Thread(target=keyin, args=())
     t2.daemon = True
     t2.start()
+    #THIS LOOP IS NEEDED TO KEEP YOUR THREADS ALIVE
     while True:
         time.sleep(1)
