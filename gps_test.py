f3e893b3eb5baecd6642de1eef162b6b5865b37c
rmisra077 <rmisra077@gmail.com>
2018-07-31 10:08:17 -0400

    Basic methods added to gps_test

--- gps_test.py
+++ gps_test.py
@@ -1,10 +1,11 @@
 import serial
 import time
 import sys
-import logging
-
+from submodules import *
 from threading import Thread
 from core import config
+import logging
+
 
 #EDIT THIS TO WORK WITH GPS
 
@@ -16,6 +17,46 @@ def on_startup():
     t1 = Thread(target=listen, args=())
     t1.daemon = True
     t1.start()
+
+def send(msg):
+    #Simple Message Sending with serial
+    msg += "\n"
+    ser.write(msg.encode("utf-8"))
+def listen():
+    while(True):
+        zz = ser.inWaiting()
+        rr = ser.read(size = zz)
+        if zz > 0:
+            time.sleep(.5)
+            zz = ser.inWaiting()
+            rr += ser.read(size = zz)
+            print(rr)
+            log('GOT: '+rr)
+def keyin():
+    while(True):
+        #GET INPUT FROM YOUR OWN TERMINAL
+        #TRY input("shihaoiscoolforcommentingstuff") IF raw_input() doesn't work
+        in1 = input("Type Command: ")
+        send("TJ" + in1 + chr(sum([ord(x) for x in "TJ" + in1]) % 128))
+def on_startup():
+    #GLOBAL VARIABLES ARE NEEDED IF YOU "CREATE" VARIABLES WITHIN THIS METHOD
+    #AND ACCESS THEM ELSEWHERE
+    global bperiod, t1, ser, logfile
+    bperiod = 60
+    #serialPort = config['aprs']['serial_port']
+    #REPLACE WITH COMx IF ON WINDOWS
+    #REPLACE WITH /dev/ttyUSBx if 1 DOESNT WORK
+    serialPort = "/dev/ttyUSB0"
+    #OPENS THE SERIAL PORT FOR ALL METHODS TO USE WITH 19200 BAUD
+    ser = serial.Serial(serialPort, 19200)
+    #CREATES A THREAD THAT RUNS THE LISTEN METHOD
+    t1 = Thread(target=listen, args=())
+    t1.daemon = True
+    t1.start()
+    tlt = time.localtime()
+    filename = 'gps'+'-'.join([str(x) for x in tlt[0:3]])
+    logfile = open('/home/pi/TJREVERB/pFS/submodules/logs/gps/'+filename+'.txt','a+')
+    log('RUN@'+'-'.join([str(x) for x in tlt[3:5]]))
 
 def enter_normal_mode():
     global bperiod
