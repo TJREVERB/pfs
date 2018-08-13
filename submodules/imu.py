56e8a383762a489dbc7aa9e2526e5b123f79419b
Shihao <shihaocao@gmail.com>
2018-08-13 14:48:40 -0400

    reindent

--- submodules/imu.py
+++ submodules/imu.py
@@ -5,58 +5,58 @@ from threading import Thread
 bus = smbus.SMBus(1)
 address = 0x68
 def imu_beacon():
-	global datasave
-	while True:
-		if len(datasave) > 9:
-			for x in datasave:
-				aprs.send(x)
-		datasave = []
-		time.sleep(1)
+    global datasave
+    while True:
+        if len(datasave) > 9:
+            for x in datasave:
+                aprs.send(x)
+        datasave = []
+        time.sleep(1)
 def acc():
-        global ax, ay, az, datasave
-        ax=bus.read_byte_data(address, 0x3B)
-        ay=bus.read_byte_data(address, 0x3D)
-        az=bus.read_byte_data(address, 0x3F)
-        # print("acc", ax, ay, az)
+    global ax, ay, az, datasave
+    ax=bus.read_byte_data(address, 0x3B)
+    ay=bus.read_byte_data(address, 0x3D)
+    az=bus.read_byte_data(address, 0x3F)
+    # print("acc", ax, ay, az)
 
 def gyr():
-	global gx, gy, gz, datasave
-        gx=bus.read_byte_data(address, 0x43)
-        gy=bus.read_byte_data(address, 0x45)
-        gz=bus.read_byte_data(address, 0x47)
-        # print("gyr", gx, gy, gz)
+    global gx, gy, gz, datasave
+    gx=bus.read_byte_data(address, 0x43)
+    gy=bus.read_byte_data(address, 0x45)
+    gz=bus.read_byte_data(address, 0x47)
+    # print("gyr", gx, gy, gz)
 
 
 def acc_gyr():
-	global speriod, datasave
-	while True:
-        	acc()
-        	gyr()
-		datapoint = ':'.join([ax,ay,az,gx,gy,gz])
-        	datasave += [datapoint]
-		time.sleep(speriod)
+    global speriod, datasave
+    while True:
+        acc()
+        gyr()
+        datapoint = ':'.join([ax,ay,az,gx,gy,gz])
+        datasave += [datapoint]
+        time.sleep(speriod)
 
 def on_startup():
-	global speriod, datasave
-	enter_normal_mode()
-	t1= Thread(target = acc_gyr,args=())
-	t1.daemon=True
-	t1.start()
-	t2 = Thread(target = imubeacon, args=())
-	t2.daemon = True
-	t2.start()
+    global speriod, datasave
+    enter_normal_mode()
+    t1= Thread(target = acc_gyr,args=())
+    t1.daemon=True
+    t1.start()
+    t2 = Thread(target = imubeacon, args=())
+    t2.daemon = True
+    t2.start()
 
 def enter_normal_mode():
-	global speriod
-	speriod =2
+    global speriod
+    speriod =2
 def enter_low_power_mode():
-	global speriod
-	speriod =20
+    global speriod
+    speriod =20
 def enter_emergency_mode():
-	global speriod
-	speriod=60
+    global speriod
+    speriod=60
 
 if __name__=='__main__':
-	on_startup()
-	while True:
-		time.sleep(1)
+    on_startup()
+    while True:
+        time.sleep(1)
