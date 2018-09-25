c84442c780ff52f9256f029fd4060e1dc6a5d719
Jason Chen <jay.chen135@gmail.com>
2018-09-25 13:11:50 -0400

    working eps_interface w/ code in submodules jason 9/25

--- submodules/eps.py
+++ submodules/eps.py
@@ -2,6 +2,7 @@ import logging
 import os
 import time
 import smbus
+from smbus2 import SMBusWrapper
 from threading import Thread
 from typing import Union
 
@@ -25,7 +26,8 @@ bperiod = 60
 ser: Union[serial.Serial, None] = None
 
 address = 43
-b = smbus.SMBus(1)
+bus = smbus.SMBus(1)
+SW0 = 2 # Is this necessary? Is SW0 just for the fake eps?
 
 
 def listen():
@@ -35,33 +37,43 @@ def listen():
         # Dispatch command
         parse_aprs_packet(line)
 def pin_on(PDM_val):
-    b.write_i2c_block_data(address, 0x12, PDM_val)
+    with SMBusWrapper(1) as bus:
+        PDM_val = [PDM_val]
+        bus.write_i2c_block_data(address, 0x12, PDM_val)
 def pin_off(PDM_val):
-    b.write_i2c_block_data(address, 0x13, PDM_val)
+    with SMBusWrapper(1) as bus:
+        PDM_val = [PDM_val]
+        bus.write_i2c_block_data(address, 0x13, PDM_val)
 def get_board_status():
-    return b.read_i2c_block_data(address, 0x01)
+    with SMBusWrapper(1) as bus:
+        return bus.read_i2c_block_data(address, 0x01)
 def set_system_watchdog_timeout(timeout):
-    b.write_i2c_block_data(address, 0x06, timeout)
+    with SMBusWrapper(1) as bus:
+        timeout = [timeout]
+        bus.write_i2c_block_data(address, 0x06, timeout)
 def get_BCR1_volts():
-    b.write_i2c_block_data(address, 0x10, 0x00)
-    return b.read_byte(address)
+    with SMBusWrapper(1) as bus:
+        bus.write_i2c_block_data(address, 0x10, 0x00)
+        return bus.read_byte(address)
 def get_BCR1_amps_A():
-    b.write_i2c_block_data(address, 0x10, 0x01)
-    return b.read_byte(address)
+    with SMBusWrapper(1) as bus:
+        bus.write_i2c_block_data(address, 0x10, 0x01)
+        return bus.read_byte(address)
 def get_BCR1_amps_B():
-    b.write_i2c_block_data(address, 0x10, 0x02)
-    return b.read_byte(address)
+    with SMBusWrapper(1) as bus:
+        bus.write_i2c_block_data(address, 0x10, 0x02)
+        return bus.read_byte(address)
 def led_on_off():
     while true:
         pin_on(SW0)
-        time.sleep(2)
+        time.sleep(1)
         pin_off(SW0)
-        time.sleep(2)
+        time.sleep(1)
 
 # Method that is called upon application startup.
 def on_startup():
     global ser, logfile
-    global address, b
+    global address, bus, SW0
 
     # Opens the serial port for all methods to use with 19200 baud
     #ser = serial.Serial(config['eps']['serial_port'], 19200)
