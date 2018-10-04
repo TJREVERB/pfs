2d5beebbb032759a8a687cfccfe40956382a5068
Will Kohn <willkohn@gmail.com>
2018-10-04 18:42:31 -0400

    hi

--- submodules/iridium.py
+++ submodules/iridium.py
@@ -1,6 +1,20 @@
+import sys
+import serial
+import time
+import os
+
+
+debug = True
+global ser
+
 #!/usr/bin/env python3
+
+sys.path.append(os.path.abspath("C:/users/will kohn/documents/TJREVERB/iridium-jacob"))
+from iridiumNotTest import *
 
 
 # IRIDIUM PLACEHOLDER UNTIL JACOB DOES HIS JOB
 def enqueue(message):
     pass
+# # print("Listening for Ring")
+listenUp()
