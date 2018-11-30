65483d9768ff1d0d5cf3fdbf22d33c7c0368f23d
Dylan Jones <dylanjones2011@gmail.com>
2018-11-30 15:12:50 -0500

    Attempt at merging in Ethan's telemetry changes - needs more work

--- submodules/telemetry.py
+++ submodules/telemetry.py
@@ -1,44 +1,48 @@
-#pushed 10/10/18 by Patrick Zhang, uses placeholder variables in other files
-
+# uses placeholder variables in other files. TODO: make it use actual values
 import base64
+import logging
 import struct
-import threading
 import time
+from functools import partial
+from threading import Lock
 
 import core, sys
 from core import config
-from . import command_ingest
-from . import eps
+from core.threadhandler import ThreadHandler
+from . import adcs
+from . import aprs
 from . import gps
+from . import iridium
 from . import radio_output
 from . import aprs
 from . import adcs
 from . import iridium
 
-import time
-import logging
+packet_buffer = []  # telem packet list TODO: much better handling of telemetry packets
+packet_lock = Lock()  # TODO: Use an indexed system so that we have persistent log storage and querying
+logger = logging.getLogger("Telemetry")
 
-listTelemPackets = []   # telem packet list
-sendingPackets = False  # don't add packets while sending them
-logger = logging.getLogger("TELEM")
 
 def telemetry_collection():
-    global listTelemPackets, lastGPSsubpacket, lastCommssubpacket, lastADCSsubpacket
+    global packet_buffer
     while True:
+        # TODO: aggregate and prioritize
         # Collect subpackets, aggregate, and prioritize
-	    #GPS
-        if(time.time()%config['telemetry']['subpackets']['gps']['interval'] < 1 and sendingPackets == False and time.time() % config['telemetry']['send_interval'] > 5):
-            lastGPSsubpacket = gps_subpacket()
-            listTelemPackets.append(gps_subpacket())
+        # Aquire the send lock so that we don't add packets while bursting
+        packet_lock.acquire()
+        # GPS
+        if time.time() % config['telemetry']['subpackets']['gps']['interval'] < 1:
+            packet_buffer.append(gps_subpacket())
         # Comms
-        if(time.time()%config['telemetry']['subpackets']['comms']['interval'] < 1 and sendingPackets == False and time.time() % config['telemetry']['send_interval'] > 5):
-            lastCommssubpacket = comms_subpacket()
-            listTelemPackets.append(lastCommssubpacket)
+        if time.time() % config['telemetry']['subpackets']['comms']['interval'] < 1:
+            packet_buffer.append(comms_subpacket())
         # ADCS
-        if(time.time()%config['telemetry']['subpackets']['adcs']['interval'] < 1 and sendingPackets == False and time.time() % config['telemetry']['send_interval'] > 5):
-            lastADCSsubpacket = adcs_subpacket()
-            listTelemPackets.append(lastADCSsubpacket)
+        if time.time() % config['telemetry']['subpackets']['adcs']['interval'] < 1:
+            packet_buffer.append(adcs_subpacket())
+        # logger.debug("Packet Buffer is %d long" % len(packet_buffer))
+        packet_lock.release()
         time.sleep(1)
+
 
 def telemetry_send():
     while True:
@@ -48,51 +52,54 @@ def telemetry_send():
             logger.debug("Sent " + str(beg_count - len(listTelemPackets)) + " telemetry packets")
         time.sleep(1)
 
+
 def gps_subpacket():
-    #packet header
-	packet = "G"
-	#Time
-	packet+=str(base64.b64encode(struct.pack('f',time.time())))
-	#GPS coords
-	packet+=str(base64.b64encode(struct.pack('fff',gps.lat,gps.lon,gps.alt)))
-	#radio_output.send_immediate_raw(packet)
-	return packet
+    # packet header
+    packet = "G"
+    # Time
+    packet += str(base64.b64encode(struct.pack('f', time.time())))
+    # GPS coords
+    packet += str(base64.b64encode(struct.pack('fff', gps.lat, gps.lon, gps.alt)))
+    # radio_output.send_immediate_raw(packet)
+    return packet
 
 
 def adcs_subpacket():
-	#packet header
-	packet = "A"
-	#time
-	packet+=str(base64.b64encode(struct.pack('f',time.time())))
-	#pitch,roll,yaw
-	pitch,roll,yaw = adcs.get_pry()
-	packet+=str(base64.b64encode(struct.pack("ddd",pitch,roll,yaw)))
-	#absolute x,y,z
-	absx,absy,absz = adcs.get_abs()
-	packet+=str(base64.b64encode(struct.pack("fff",absx,absy,absz)))
-	#mag x,y,z
-	magx,magy,magz = adcs.get_mag()
-	packet+=str(base64.b64encode(struct.pack("ddd",magx,magy,magz)))
-	#radio_output.send_immediate_raw(packet)
-	return packet
+    # packet header
+    packet = "A"
+    # time
+    packet += str(base64.b64encode(struct.pack('f', time.time())))
+    # pitch,roll,yaw
+    pitch, roll, yaw = adcs.get_pry()
+    packet += str(base64.b64encode(struct.pack("ddd", pitch, roll, yaw)))
+    # absolute x,y,z
+    absx, absy, absz = adcs.get_abs()
+    packet += str(base64.b64encode(struct.pack("fff", absx, absy, absz)))
+    # mag x,y,z
+    magx, magy, magz = adcs.get_mag()
+    packet += str(base64.b64encode(struct.pack("ddd", magx, magy, magz)))
+    # radio_output.send_immediate_raw(packet)
+    return packet
+
 
 def comms_subpacket():
-	#packet header
-	packet = "C"
-	#Time
-	packet+=str(base64.b64encode(struct.pack('f',time.time())))
-	#APRS info
-	packet+=str(base64.b64encode(struct.pack('d',aprs.total_received_ph)))
-	packet+=str(base64.b64encode(struct.pack('d',aprs.success_checksum_ph)))
-	packet+=str(base64.b64encode(struct.pack('d',aprs.fail_checksum_ph)))
-	packet+=str(base64.b64encode(struct.pack('d',aprs.sent_messages_ph)))
-	#IRIDIUM info
-	packet+=str(base64.b64encode(struct.pack('d',iridium.total_received_ph)))
-	packet+=str(base64.b64encode(struct.pack('d',iridium.success_checksum_ph)))
-	packet+=str(base64.b64encode(struct.pack('d',iridium.fail_checksum_ph)))
-	packet+=str(base64.b64encode(struct.pack('d',iridium.sent_messages_ph)))
-	#radio_output.send_immediate_raw(packet)
-	return packet
+    # packet header
+    packet = "C"
+    # Time
+    packet += str(base64.b64encode(struct.pack('f', time.time())))
+    # APRS info
+    packet += str(base64.b64encode(struct.pack('d', aprs.total_received_ph)))
+    packet += str(base64.b64encode(struct.pack('d', aprs.success_checksum_ph)))
+    packet += str(base64.b64encode(struct.pack('d', aprs.fail_checksum_ph)))
+    packet += str(base64.b64encode(struct.pack('d', aprs.sent_messages_ph)))
+    # IRIDIUM info
+    packet += str(base64.b64encode(struct.pack('d', iridium.total_received_ph)))
+    packet += str(base64.b64encode(struct.pack('d', iridium.success_checksum_ph)))
+    packet += str(base64.b64encode(struct.pack('d', iridium.fail_checksum_ph)))
+    packet += str(base64.b64encode(struct.pack('d', iridium.sent_messages_ph)))
+    # radio_output.send_immediate_raw(packet)
+    return packet
+
 
 def system_subpacket():
     pass
@@ -111,25 +118,26 @@ def last_adcs_subpacket():
 
 # Sends the queued packets through radio_output
 def send():
-    global listTelemPackets, sendingPackets
-    sendingPackets = True
-    for packet in listTelemPackets:
-        radio_output.send(packet, None) # radio is set to default; change if necessary
-        time.sleep(1)
-        listTelemPackets.remove(packet)
-    sendingPackets = False
+    global packet_buffer
+    packet_lock.acquire()
+    for packet in packet_buffer:
+        radio_output.send(packet, None)  # radio is set to default; change if necessary
+        # packet_buffer.remove(packet) <<< should cause a ConcurrentModificaitionException
+        logger.debug(len(packet_buffer))
+    logger.debug("Done dumping packets")
+    packet_lock.release()
 
 
 def on_startup():
-    t = threading.Thread(target=telemetry_collection, daemon=True)
-    t.start()
+    t1 = ThreadHandler(target=partial(telemetry_collection), name="telemetry-telemetry_collection")
+    t1.start()
 
-    t2 = threading.Thread(target=telemetry_send, daemon=True)
+    t2 = ThreadHandler(target=partial(telemetry_send), name="telemetry-telemetry_send")
     t2.start()
 
 
 def enter_emergency_mode():
-    pass
+    pass  # TODO: change config
 
 
 def enter_low_power_mode():
