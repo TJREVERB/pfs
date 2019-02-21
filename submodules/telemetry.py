2da759f06f8e2d217f6494495655a9ba2b52de8d
Ethan Nguyen <etnguyen03@hotmail.com>
2019-02-21 17:19:53 -0500

    enqueue submodule packet

--- submodules/telemetry.py
+++ submodules/telemetry.py
@@ -26,46 +26,13 @@ packetBuffers = [event_packet_buffer, telem_packet_buffer]
 packet_lock = Lock()  # TODO: Use an indexed system so that we have persistent log storage and querying
 logger = logging.getLogger("Telemetry")
 
-
-def telemetry_collection():
-    global telem_packet_buffer
-    while True:
-        with packet_lock:
-            # TODO: aggregate and prioritize
-            # Collect subpackets, aggregate, and prioritize
-            # Aquire the send lock so that we don't add packets while bursting
-            #packet_lock.acquire()
-            # GPS
-            #if time.time() % config['telemetry']['subpackets']['gps']['interval'] < 1:
-            try:
-                telem_packet_buffer.append(gps_subpacket())
-            except Exception as err:
-                logger.error("Exception: " + str(err)) #TODO: handle exceptions better
-            # Comms
-            #if time.time() % config['telemetry']['subpackets']['comms']['interval'] < 1:
-            try:
-                telem_packet_buffer.append(comms_subpacket())
-            except Exception as err:
-                logger.error("Exception: " + str(err))
-            # ADCS
-            #if time.time() % config['telemetry']['subpackets']['adcs']['interval'] < 1:
-            try:
-                telem_packet_buffer.append(adcs_subpacket())
-            except Exception as err:
-                logger.error("Exception: " + str(err))
-            # logger.debug("Packet Buffer is %d long" % len(packet_buffer))
-            #packet_lock.release()
-        time.sleep(config['telemetry']['subpackets'])
-        #print(telem_packet_buffer)
-
-
 def telemetry_send():
     """
     Thread method to burst the telemetry every so often
     """
-    # TODO: check battery levels
+    # TODO: check battery levels before sending
     global telem_packet_buffer, event_packet_buffer
-    time.sleep(60)
+    time.sleep(60)  # Don't send packets straight away
 
     while True:
         if (adcs.can_TJ_be_seen() == True and len(telem_packet_buffer) + len(event_packet_buffer) > 0):
@@ -84,85 +51,18 @@ def telemetry_send_once():
 @command("burst")
 def telemetry_burst_command():
     """
-    Burst command; ignores isTJseen and other checks
+    Burst command; ignores isTJseen and other checks.
     """
-    send(ignoreAPRS=True)
-
-def gps_subpacket():
-    """
-    Return a GPS subpacket
-    """
-    # packet header
-    packet = "G"
-    # Time
-    packet += base64.b64encode(struct.pack('d', time.time())).decode('UTF-8')
-    # GPS coords
-    # TODO fix this packet += base64.b64encode(struct.pack('fff', gps.lat, gps.lon, gps.alt)).decode('UTF-8')
-    packet += base64.b64encode(struct.pack('fff', -1, -1, -1)).decode('UTF-8')
-    # radio_output.send_immediate_raw(packet)
-
-    return packet
-
-
-def adcs_subpacket():
-    """
-    Return an ADCS subpacket
-    """
-    # packet header
-    packet = "A"
-    # time
-    packet += base64.b64encode(struct.pack('d', time.time())).decode('UTF-8')
-    # pitch,roll,yaw
-    pitch, roll, yaw = adcs.get_pry()
-    packet += base64.b64encode(struct.pack("ddd", pitch, roll, yaw)).decode('UTF-8')
-    # absolute x,y,z
-    absx, absy, absz = adcs.get_abs()
-    packet += base64.b64encode(struct.pack("fff", absx, absy, absz)).decode('UTF-8')
-    # mag x,y,z
-    magx, magy, magz = adcs.get_mag()
-    packet += base64.b64encode(struct.pack("ddd", magx, magy, magz)).decode('UTF-8')
-    # radio_output.send_immediate_raw(packet)
-
-    return packet
-
-
-def comms_subpacket():
-    """
-    Return a comms subpacket
-    """
-    # packet header
-    packet = "C"
-    # Time
-    packet += base64.b64encode(struct.pack('d', time.time())).decode('UTF-8')
-    # Sequence number
-    #packet += base64.b64encode(struct.pack('i', comms_sequence_number)).decode('UTF-8')
-    # APRS info
-    packet += base64.b64encode(struct.pack('d', aprs.total_received_ph)).decode('UTF-8')
-    packet += base64.b64encode(struct.pack('d', aprs.success_checksum_ph)).decode('UTF-8')
-    packet += base64.b64encode(struct.pack('d', aprs.fail_checksum_ph)).decode('UTF-8')
-    packet += base64.b64encode(struct.pack('d', aprs.sent_messages_ph)).decode('UTF-8')
-    # IRIDIUM info
-    packet += base64.b64encode(struct.pack('d', iridium.total_received_ph)).decode('UTF-8')
-    packet += base64.b64encode(struct.pack('d', iridium.success_checksum_ph)).decode('UTF-8')
-    packet += base64.b64encode(struct.pack('d', iridium.fail_checksum_ph)).decode('UTF-8')
-    packet += base64.b64encode(struct.pack('d', iridium.sent_messages_ph)).decode('UTF-8')
-    # radio_output.send_immediate_raw(packet)
-    return packet
-
-#TODO: add in system subpackets
-def system_subpacket():
-    pass
-
-#TODO: EPS subpacket
+    send(ignoreADCS=True)
 
 def enqueue_event_message(event):
     """
     Enqueue an event message.
-    event - message to enqueue, **MUST BE EXACTLY** 16 bytes
+    :param event: message to enqueue, **MUST BE EXACTLY** 16 bytes
     """
 
     if len(event.encode('utf-8')) != 16:
-        logger.error("Event message must be exactly 16 bytes, message is " + str(len(event.encode('utf-8'))) + " bytes")
+        logger.error("Event message must be exactly 16 bytes, message is " + str(len(event.encode('utf-8'))) + " bytes long")
         return
 
     global event_packet_buffer
@@ -171,11 +71,32 @@ def enqueue_event_message(event):
     packet += event
     event_packet_buffer.append(packet)
 
+def enqueue_submodule_packet(packet):
+    """
+    Accepts a single raw string packet and enqueues them into the telemetry packet buffer.
+    :param packet: A correctly formatted string subpacket to enqueue.
+    """
+    global telem_packet_buffer, packet_lock
+    with packet_lock:
+        telem_packet_buffer.append(packet)
+
+def enqueue_submodule_packets(packets):
+    """
+    Accepts raw string packets and enqueues them into the telemetry packet buffer.
+    :param packets: A list of string subpackets to enqueue. These packets must be formatted correctly
+    """
+    global telem_packet_buffer, packet_lock
+    with packet_lock:
+        for packet in packets:
+            telem_packet_buffer.append(packet)
+
+
 def send(ignoreADCS=False):
     """
     Concatenates packets to fit in max_packet_size (defined in config) and send through the APRS, dequing the packets in the process
+    :param ignoreADCS: If true, ignores ADCS canTJBeSeen.
     """
-    global packetBuffers, event_packet_buffer, telem_packet_buffer
+    global packetBuffers, event_packet_buffer, telem_packet_buffer, packet_lock
     squishedPackets = ""
 
     with packet_lock:
@@ -198,16 +119,11 @@ def send(ignoreADCS=False):
 
 def start():
     """
-    Starts the telemetry collection and telemetry send threads
+    Starts the telemetry send thread
     """
-    t1 = ThreadHandler(target=partial(telemetry_collection), name="telemetry-telemetry_collection")
-    t1.start()
 
     t2 = ThreadHandler(target=partial(telemetry_send), name="telemetry-telemetry_send")
     t2.start()
-
-
-    print(gps.get_position_packet())
 
 # TODO: Need to know what needs to be done in low power and emergency modes.
 def enter_emergency_mode():
