5a308cfdc1593736a5658975987bf3fa37a818b9
Will Kohn <willkohn@gmail.com>
2019-02-22 15:50:48 -0500

    save change in response string

--- submodules/iridium.py
+++ submodules/iridium.py
@@ -33,12 +33,12 @@ def write_to_serial(cmd):
 
     # Determine if an "OK" or an "ERROR" was received
     if "OK" in response:
-        response.replace("OK", "")
-        response.replace("/r/n", "")
+        response=response.replace("OK", "")
+        response=response.replace("/r/n", "")
         ser.flush()  # Flush the serial
         return response, True
     else:
-        response.replace("ERROR", "").strip()
+        response=response.replace("ERROR", "")
         ser.flush()  # Flush the serial
         return response, False
 
@@ -59,7 +59,7 @@ def check(numChecks):
     write_to_serial("AT+SBDMTA=0")
 
     # Get the current registration status of the Iridium
-    response = int(write_to_serial("AT+SBDREG?")[0].split(":")[1]) # Returning OK instead of 2, add check for these types of commands in write_to_serial
+    response = int(write_to_serial("AT+SBDREG?")[0].split(":")[1]) # Returning OK and endlines when they should be removed in write_to_serial
 
     # `response` should be 2, which means the Iridium is registered
     while numChecks > 0:  # Recheck the Iridium for `numChecks` number of times
