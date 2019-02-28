e0dc89a238d66d5f3d8d614006ad3cf6b0fda247
Akash Bhave <akashbbhave@gmail.com>
2019-02-28 17:26:27 -0500

    Cleanup send() in iridium

--- submodules/iridium.py
+++ submodules/iridium.py
@@ -13,19 +13,19 @@ global ser
 logger = logging.getLogger("IRIDIUM")
 
 
-def write_to_serial(cmd):
+def write_to_serial(command):
     """
     Write a command to the serial port.
 
-    :param cmd: Command to write
+    :param command: Command to write
     :return: Tuple consisting of (response text, boolean if error or not)
     """
 
-    # Append EOL if it isn't present alredy
-    if cmd[-2:] != '\r\n':
-        cmd += '\r\n'
+    # Append EOL if it isn't present already
+    if command[-2:] != '\r\n':
+        command += '\r\n'
 
-    ser.write(cmd.encode('UTF-8'))  # Encode the message with utf-8
+    ser.write(command.encode('UTF-8'))  # Encode the message with utf-8
 
     response = ""  # Received response
     while ("OK" or "ERROR") not in response:  # Wait to get the 'OK' or 'ERROR' from Iridium
@@ -33,20 +33,19 @@ def write_to_serial(cmd):
 
     # Determine if an "OK" or an "ERROR" was received
     if "OK" in response:
-        response=response.replace("OK", "")
-        response=response.replace("/r/n", "")
+        response = response.replace("OK", "").strip()
         ser.flush()  # Flush the serial
         return response, True
     else:
-        response=response.replace("ERROR", "")
+        response = response.replace("ERROR", "").strip()
         ser.flush()  # Flush the serial
         return response, False
 
 
-def check(numChecks):
+def check(num_checks):
     """
     Check that the Iridium works and is registered
-    :param numChecks: Number of times to check if the Iridium is registered
+    :param num_checks: Number of times to check if the Iridium is registered
     :return: True if check was successful, False if not
     """
 
@@ -59,15 +58,16 @@ def check(numChecks):
     write_to_serial("AT+SBDMTA=0")
 
     # Get the current registration status of the Iridium
-    response = int(write_to_serial("AT+SBDREG?")[0].split(":")[1]) # Returning OK and endlines when they should be removed in write_to_serial
+    # Return OK and end lines when they should be removed in write_to_serial
+    response = int(write_to_serial("AT+SBDREG?")[0].split(":")[1])
 
     # `response` should be 2, which means the Iridium is registered
-    while numChecks > 0:  # Recheck the Iridium for `numChecks` number of times
+    while num_checks > 0:  # Recheck the Iridium for `num_checks` number of times
         if response == 2:  # Check succeeded
             return True
         else:  # Check failed, retry
-            response = write_to_serial("AT+SBDREG")[0]
-            numChecks -= 1
+            response = int(write_to_serial("AT+SBDREG?")[0].split(":")[1])
+            num_checks -= 1
     return False  # Check failed all times, return False
 
 
@@ -115,31 +115,30 @@ def listen():
 
 
 def send(message):
-    # Try to send until it sends
-    startTime = time.time()
+    """
+    Send a message using the Iridium network.
+    :param message: The message to send in plain text
+    :return:
+    """
+
     alert = 2
     while alert == 2:
         # Get last known signal strength
-        write_to_serial("AT+CSQF")
+        write_to_serial("AT+CSQ")
 
         # Prepare message
-        write_to_serial("AT+SBDWT=" + message)
+        response_write = write_to_serial("AT+SBDWT=" + message)  # Message write timed out
+        if not response_write[1]: return False
 
         # Send message
-        response = write_to_serial("AT+SBDI").replace(",", " ")
+        response_sbdi = write_to_serial("AT+SBDI")
+        if not response_sbdi[1]: return False
+        response_sbdi_array = response_sbdi[0].split(":")[1].strip().split(",")  # Array of SBDI response
 
-        startTime = time.time()
-        currTime = startTime
-
-        while len(response) > 0 and len(response) <= 2:
-            response = write_to_serial("AT+SBDI").replace(",", " ")
-            curTime = time.time()
-            if (curTime - startTime) > 30:
-                break
-        try:
-            alert = int(response[1])
-        except:
-            continue
+        if response_sbdi_array[0] == 2:
+            return True
+        else:
+            return False
 
 
 def start():
@@ -147,11 +146,11 @@ def start():
 
     # Opens the serial port for all methods to use with 19200 baud
     ser = serial.Serial(
-        config['iridium']['serial_port'], baudrate=19200, timeout=15)
+        config['iridium']['serial_port'], baudrate=19200, timeout=120)
     # Clean serial port before proceeding
     ser.flush()
 
-    check(5)  # Check that the Iridium works 5 times
+    check(5)  # Check that the Iridium (check 5 times)
 
     # Create all the background threads
     # t1 = ThreadHandler(target=partial(listen),name="iridium-listen", parent_logger=logger)
@@ -161,4 +160,3 @@ def start():
     # t1.start() threads break serial
 
     time.sleep(1)
-    send("TEST")
