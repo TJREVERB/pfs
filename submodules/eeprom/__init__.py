6454d6711c3ed5f98854b907b037b97c45ddbc8e
abagali1 <anup.bagali1@gmail.com>
2019-04-19 10:55:28 -0400

    reverted eeprom changes

--- submodules/eeprom/__init__.py
+++ submodules/eeprom/__init__.py
@@ -1,4 +1,6 @@
-from create_yaml import make_file
+from .eeprom_read_yaml import read_yaml
+from .eeprom_write_yaml import write_yaml
+from .create_yaml import make_file
 
 def start():
   make_file()
@@ -8,7 +10,7 @@ def store_yaml(address=0x50, filename="config.yaml"):
   
 def regen_yaml(address=0x50, size=1024):
   read_yaml(address, size)
-  
+
 def count(address=0x50):
   read_yaml(address, 16384)
 
