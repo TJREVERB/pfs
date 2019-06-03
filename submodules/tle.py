06b5b511bc49e339ee0af4314d3f698e8a85c2df
ayusher <42046470+ayusher@users.noreply.github.com>
2019-06-03 15:39:57 -0400

    Add needed functions

--- submodules/tle.py
+++ submodules/tle.py
@@ -6,10 +6,18 @@ def load():
     orb = Orbital("TJREVERB", tle_file=(
         Path(__file__).parent.resolve() / "tjreverb_tle.txt"))
 
-def get_lla(t):
+def get_lla(t): #t is a datetime object
     load()
     return({'lat': orb.get_lonlatalt(t)[0], 'lon': orb.get_lonlatalt(t)[1], 'alt': orb.get_lonlatalt(t)[2]})
 
-def get_xyz(t):
+def get_xyz(t): #t is a datetime object
     load()
     return({'xyz_pos': orb.get_position(t)[0], 'xyz_vel': orb.get_position(t)[1]})
+
+def can_TJ_be_seen(t): #t is a datetime object
+    load()
+    a=orb.get_next_passes(t, 1, -77.1687977, 38.8183519, .08, tol=0.001, horizon=0)
+    return(a[0][0]<=t and t<=a[0][1])
+
+def get_next_passes(t): #t is a datetime object
+        return(orb.get_next_passes(t, 1, -77.1687977, 38.8183519, .08, tol=0.001, horizon=0))
