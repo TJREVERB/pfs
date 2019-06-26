a8d4b90219cd8440852b2fdb1646b7158a1dddf2
Akash Bhave <akashbbhave@gmail.com>
2019-06-26 12:38:01 -0400

    Rename format script to reformat to avoid shadowing

--- tasks.py
+++ tasks.py
@@ -14,12 +14,12 @@ def run(c, production=False, debug=False):
 
 
 @task
-def format(c):
+def reformat(c):
     c.run("pipenv run autopep8 --in-place -r ./")
 
 
 @task
 def deploy(c):
     c.run("pipenv lock -r > requirements.txt")
-    c.run("pip3 install -r requirements.txt")
+    c.run("sudo pip3 install -r requirements.txt")
     os.remove("requirements.txt")
