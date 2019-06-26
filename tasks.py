abe12e5c0be4ddc5a051be74b192ceb9bb778499
Akash Bhave <akashbbhave@gmail.com>
2019-06-26 14:30:19 -0400

    Use apt instead of pip for certain packages in deploy task

--- tasks.py
+++ tasks.py
@@ -20,6 +20,36 @@ def reformat(c):
 
 @task
 def deploy(c):
+    # Lock the requirements in a file
     c.run("pipenv lock -r > requirements.txt")
-    c.run("sudo pip3 install -r requirements.txt")
+
+    with open("requirements.txt", "r") as file_locked:
+        requirements = file_locked.readlines()
+    file_locked.close()
     os.remove("requirements.txt")
+
+    # It's much quicker to use the Debian packages to install these vs. apt
+    requirements_skip = ["astropy", "numpy", "scipy"]
+    requirements_skip_to_install = []
+
+    # Remove the unwanted packages in requirements.txt, add their package names to list
+    with open("requirements.txt", "w") as file_new:
+        for pos, line in enumerate(requirements):
+            write_line = True
+            for r in requirements_skip:
+                if r in line:
+                    requirements_skip_to_install.append("python3-" + r)
+                    write_line = False
+
+            if write_line:
+                file_new.write(line)
+
+    file_new.close()
+
+    # Install requirements.txt using pip
+    c.run("sudo pip3 install --verbose -r requirements.txt")
+    os.remove("requirements.txt")
+
+    # Install skipped packages using apt
+    for r in requirements_skip_to_install:
+        c.run("sudo apt install " + r)
