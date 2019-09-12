7f3bdf1f915afbf67e43fbc87f14d940362b7876
Akash Bhave <akashbbhave@gmail.com>
2019-09-12 17:11:43 -0400

    Make start() a multi-line string, add comments

--- submodules/command_ingest/__init__.py
+++ submodules/command_ingest/__init__.py
@@ -52,7 +52,7 @@ def dispatch(body: str) -> None:
     """
     global total_errors, total_received, total_success
     # TODO: exception proof this (index out of bounds)
-    if body[0:2] == 'TJ' and body[-5:-1] == '\\r\\n':
+    if body[0:2] == 'TJ' and body[-5:-1] == '\\r\\n':  # FIXME: Remove EOL check
         # TODO: exception proof (index out of bounds)
         if generate_checksum(body) == body[-7]:
             logger.debug("Message %s passed checksum." % body)
@@ -61,6 +61,7 @@ def dispatch(body: str) -> None:
             cmd = body[2:].strip().split(",")
             if cmd[0] not in registered_commands:
                 logger.warning("Command with correct checksum not registered.")
+                total_errors += 1
                 return
             if len(registered_commands[cmd[0]]["args"]) != len(cmd) - 1:
                 logger.warning("Incorrect number of arguments for command.")
@@ -68,6 +69,7 @@ def dispatch(body: str) -> None:
                 return
             args = []
             for i, argtype in enumerate(registered_commands[cmd[0]]["args"]):
+                # TODO: Why are we appending the next command
                 try:
                     if argtype == str:  # Don't call str() on a string, as that puts extra quotes around it
                         args.append(cmd[i + 1])
@@ -95,20 +97,22 @@ def dispatch(body: str) -> None:
         # logger.debug('Invalid message')
         pass
 
-# def start():
-#     global modules, m_aprs, m_gps, m_adcs, m_eps
-#     # modules = {'A':core,'B':m_aprs,'C':'iridium','D':'housekeeping','E':'log','F':'GPS'}
-#     m_aprs = {'a': aprs.enqueue, 'b': aprs.enter_normal_mode, 'c': aprs.enter_low_power_mode,
-#               'd': aprs.enter_emergency_mode}
-#     m_gps = {'a': gps.sendgpsthruaprs, 'b': gps.queryfield, 'c': gps.querygps, 'd': gps.querypastgps,
-#              'e': gps.getsinglegps, 'f': gps.enter_normal_mode, 'g': gps.enter_low_power_mode,
-#              'h': gps.enter_emergency_mode}
-#     # m_adcs = {'a': adcs.enter_normal_mode, 'b': adcs.enter_low_power_mode, 'c': adcs.enter_emergency_mode}
-#     # m_eps = {'a': eps.pin_on, 'b': eps.pin_off, 'c': eps.get_pdm_status, 'd': eps.get_board_status, 'e': eps.set_system_watchdog_timeout, 'f': eps.get_bcr1_volts, 'g': eps.get_bcr1_amps_a, 'h': eps.get_bcr1_amps_b, 'i': eps.get_board_telem, 'j': eps.enter_normal_mode, 'k': eps.enter_low_power_mode, 'l': eps.enter_emergency_mode}
-#     # m_imu = {'a': imu.enter_normal_mode, 'b': imu.enter_low_power_mode, 'c': imu.enter_emergency_mode}
-#     # m_iridium = {}  # BLANK - nothing useful is in iridium.py
-#     # m_housekeeping = {'a': housekeeping.enter_normal_mode, 'b': housekeeping.enter_low_power_mode, 'c': housekeeping.enter_emergency_mode}
-#     # m_telemetry = {'a': telemetry.gps_subpacket, 'b': telemetry.adcs_subpacket, 'c': telemetry.comms_subpacket, 'd': telemetry.system_subpacket}
-#     modules = {'B': m_aprs, 'F': m_gps}
-#     # modules = {'B': m_aprs, 'C': m_iridium, 'D': m_housekeeping, 'F': m_gps, 'G': m_eps, 'H': m_adcs, 'I': m_imu, 'J': m_telemetry}
-#     # core = {}
+"""
+def start():
+    global modules, m_aprs, m_gps, m_adcs, m_eps
+    # modules = {'A':core,'B':m_aprs,'C':'iridium','D':'housekeeping','E':'log','F':'GPS'}
+    m_aprs = {'a': aprs.enqueue, 'b': aprs.enter_normal_mode, 'c': aprs.enter_low_power_mode,
+              'd': aprs.enter_emergency_mode}
+    m_gps = {'a': gps.sendgpsthruaprs, 'b': gps.queryfield, 'c': gps.querygps, 'd': gps.querypastgps,
+             'e': gps.getsinglegps, 'f': gps.enter_normal_mode, 'g': gps.enter_low_power_mode,
+             'h': gps.enter_emergency_mode}
+    # m_adcs = {'a': adcs.enter_normal_mode, 'b': adcs.enter_low_power_mode, 'c': adcs.enter_emergency_mode}
+    # m_eps = {'a': eps.pin_on, 'b': eps.pin_off, 'c': eps.get_pdm_status, 'd': eps.get_board_status, 'e': eps.set_system_watchdog_timeout, 'f': eps.get_bcr1_volts, 'g': eps.get_bcr1_amps_a, 'h': eps.get_bcr1_amps_b, 'i': eps.get_board_telem, 'j': eps.enter_normal_mode, 'k': eps.enter_low_power_mode, 'l': eps.enter_emergency_mode}
+    # m_imu = {'a': imu.enter_normal_mode, 'b': imu.enter_low_power_mode, 'c': imu.enter_emergency_mode}
+    # m_iridium = {}  # BLANK - nothing useful is in iridium.py
+    # m_housekeeping = {'a': housekeeping.enter_normal_mode, 'b': housekeeping.enter_low_power_mode, 'c': housekeeping.enter_emergency_mode}
+    # m_telemetry = {'a': telemetry.gps_subpacket, 'b': telemetry.adcs_subpacket, 'c': telemetry.comms_subpacket, 'd': telemetry.system_subpacket}
+    modules = {'B': m_aprs, 'F': m_gps}
+    # modules = {'B': m_aprs, 'C': m_iridium, 'D': m_housekeeping, 'F': m_gps, 'G': m_eps, 'H': m_adcs, 'I': m_imu, 'J': m_telemetry}
+    # core = {}
+"""
