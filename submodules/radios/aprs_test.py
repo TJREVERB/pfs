3e29a0ac56940b65d5065c9d15814c1f7c294f39
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-11-03 23:32:58 -0500

    writing unit tests fore APRS

---
+++ submodules/radios/aprs_test.py
@@ -0,0 +1,87 @@
+import unittest
+from pty import openpty
+from os import ttyname, read, write
+
+from .aprs import APRS
+
+config = {
+    "aprs": {
+        "serial_port": ""
+    }
+}
+
+
+class FakeTelemetry:
+    def __init__(self, expected_data):
+        self.expected_data = expected_data
+
+    def enqueue(self, message):
+        assert self.expected_data == message
+
+
+def start_fake(expected_data=""):
+    """
+    Common code to all test cases (opens a fake serial port and creates an APRS test object)
+    :param expected_data: If testing GS to pFS, what str APRS is expected to pass to Telemetry
+    :return: tuple: (APRS object, master serial device)
+    """
+    master, slave = openpty()
+    port_name = ttyname(slave)
+    config["aprs"]["serial_port"] = port_name
+
+    modules = {
+        "telemetry": FakeTelemetry(expected_data)
+    }
+    aprs = APRS(config)
+    aprs.set_modules(modules)
+    return aprs, master
+
+
+class CubeSatToGroundStation(unittest.TestCase):
+    def test_normal_str(self):
+        self.assertEqual(True, False)
+
+    def test_long_str(self):
+        self.assertEqual(True, False)
+
+    def test_short_str(self):
+        self.assertEqual(True, False)
+
+    def test_empty_str(self):
+        self.assertEqual(True, False)
+
+    def test_spaces(self):
+        self.assertEqual(True, False)
+
+    def test_escape_chars(self):
+        self.assertEqual(True, False)
+
+    def close_port(self):
+        self.assertEqual(True, False)
+
+
+class GroundStationToCubeSat(unittest.TestCase):
+    def test_normal_str(self):
+        self.assertEqual(False, True)
+
+    def test_long_str(self):
+        self.assertEqual(True, False)
+
+    def test_short_str(self):
+        self.assertEqual(True, False)
+
+    def test_empty_str(self):
+        self.assertEqual(True, False)
+
+    def test_spaces(self):
+        self.assertEqual(True, False)
+
+    def test_escape_chars(self):
+        self.assertEqual(True, False)
+
+    def close_port(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
