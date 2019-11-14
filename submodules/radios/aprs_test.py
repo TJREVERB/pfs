159203b17893ebf154fb967bfefa104877b54619
=Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-11-14 18:24:07 -0500

    ground station to pfs unit tests

--- submodules/radios/aprs_test.py
+++ submodules/radios/aprs_test.py
@@ -9,6 +9,8 @@ config = {
     }
 }
 
+WORDS = ["Wood", "Low hanging FrUit", "documentation"]
+
 
 class FakeTelemetry:
     def __init__(self, expected_data):
@@ -39,17 +41,68 @@ def start_fake(expected_data=""):
     return aprs, master
 
 
+def test_write(test_str, content):
+    aprs, master = start_fake(content)
+    aprs.start()
+
+    for c in test_str:
+        b = c.encode("utf-8")
+        write(master, b)
+
+
 def test_with_header():
-    for word in ["Wood", "Low hanging FrUit", "documentation"]:
+    for word in WORDS:
         test_str = f"header:{word}\n"
         content = f"{word}\n"
-        aprs, master = start_fake(content)
-        aprs.start()
+        test_write(test_str, content)
 
-        for c in test_str:
-            b = c.encode("utf-8")
-            write(master, b)
+
+def test_with_no_header():
+    print(f"Expect {len(WORDS)} `Incomplete APRS header!`")
+    for word in WORDS:
+        test_str = f"{word}\n"
+        content = f"{word}\n"
+        test_write(test_str, content)
+
+
+def test_long_str():
+    test_str = f"header:{'182nzfei92' * 5000}\n"
+    content = '182nzfei92' * 5000 + "\n"
+    test_write(test_str, content)
+
+
+def test_short_str():
+    test_str = f"header:i\n"
+    content = "i\n"
+    test_write(test_str, content)
+
+
+def test_empty_str():
+    test_str = "header:\n"
+    content = "\n"
+    test_write(test_str, content)
+
+
+def test_spaces():
+    test_str = "header:        \n"
+    content = "        \n"
+    test_write(test_str, content)
+
+
+def test_escape_chars():
+    test_str = "header:\t\r\t\a\n"
+    content = "\t\r\t\a\n"
+    test_write(test_str, content)
 
 
 def run_tests():
+    print("EXPECT THE APRS TESTS TO TAKE SOME TIME TO RUN\n")
+
     test_with_header()
+    test_with_no_header()
+    test_short_str()
+    test_empty_str()
+    test_spaces()
+    test_escape_chars()
+
+    test_long_str()
