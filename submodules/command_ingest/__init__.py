a324eeb617b6d364572ff420043e8b3d7cbe045f
Akash Bhave <akashbbhave@gmail.com>
2019-11-14 18:06:19 -0500

    Properly parse commands in command ingest

--- submodules/command_ingest/__init__.py
+++ submodules/command_ingest/__init__.py
@@ -32,22 +32,26 @@ class CommandIngest:
         while True:
             body = self.general_queue.pop()
 
-            # Get the individual parts of the message
-            parts = [part for part in body.split(";") if part]
-            command, arguments, checksum = parts[0], parts[1].split(","), parts[2]
+            if "CMD$" in body:
+                try:
+                    module, func, args = [part for part in body[body.find("$") + 1:].split(";") if part]
+                except ValueError:
+                    # FIXME: Possibly report error
+                    pass
 
-            if self.generate_checksum(command) == checksum:
-                associated = self.associate(command)
-                if associated is not None:
-                    associated_sig = signature(associated)
-                    # Check num args and arg types
+                if self.generate_checksum(command) == checksum:
+                    associated = self.associate(command)
+                    if associated is not None:
+                        associated_sig = signature(associated)
+                        # Check num args and arg types
 
-                    if not len(arguments) == len(associated_sig.parameters):
-                        self.modules["telemetry"].enqueue(Error(sys_name="CI", msg="Incorrect number of arguments"))
-                    try:
-                        associated(*arguments)()
-                    except:
-                        self.modules["telemetry"].enqueue(Error(sys_name="CI", msg="Bad function"))
+                        if not len(arguments) == len(associated_sig.parameters):
+                            self.modules["telemetry"].enqueue(Error(sys_name="CI", msg="Incorrect number of arguments"))
+                        try:
+                            associated(*arguments)()
+                        except:
+                            self.modules["telemetry"].enqueue(Error(sys_name="CI", msg="Bad function"))
+
             else:
                 self.modules["telemetry"].enqueue(Error(sys_name="CI", msg="Incorrect checksum"))
 
@@ -66,21 +70,6 @@ class CommandIngest:
         cmd_sum += 65
         self.logger.debug('CHECKOUT :' + chr(cmd_sum) + ";")
         return chr(cmd_sum)
-
-    @staticmethod
-    def associate(cmd: str):
-        """
-        Given a function name, return a decorator that registers that function.
-        :return: Decorator that registers function.
-        """
-
-        valid_commands = {
-            "test": lambda x, y: x + y}
-
-        if cmd in valid_commands:
-            return valid_commands.get(cmd)
-        else:
-            return None
 
     def start(self):
         for process in self.processes:
