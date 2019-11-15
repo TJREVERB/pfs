e8d19872a312a830d3f68961a1e85c5ed358326f
Anup Bagali <anup.bagali1@gmail.com>
2019-11-15 12:34:36 -0500

    more doc blocks

--- submodules/radios/__init__.py
+++ submodules/radios/__init__.py
@@ -5,10 +5,24 @@ class Radio(Submodule):
     """ Abstract radio class which both APRS and Iridium extend """
 
     def __init__(self, name, config):
+        """
+        Instantiates a new Radio instance
+        :param name: name of the radio("aprs" or "iridium")
+        :param config: dictionary of configuration data
+        """
         Submodule.__init__(self, name, config)
 
-    def send(self, message):
+    def send(self, message) -> None:
+        """
+        Sends a message through the Radio
+        :param message: message to sent
+        :return: None
+        """
         raise NotImplementedError
 
-    def listen(self):
+    def listen(self) -> None:
+        """
+        Listens for incoming message on Radio
+        :return: None
+        """
         raise NotImplementedError
