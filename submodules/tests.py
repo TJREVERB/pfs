b09fcf2dde56838e04b5c34ea7e6ef8de8a14e0d
Anup Bagali <anup.bagali1@gmail.com>
2019-11-15 13:21:04 -0500

    optimized testing

--- submodules/tests.py
+++ submodules/tests.py
@@ -1,49 +1,38 @@
-MODULES = {
-    "antenna": True,
-    "command": True,
-    "eps": True,
-    "radios.aprs": True,
-    "radios.iridium": True,
-    "telemetry": True
-}
-
+MODULES = {}
 # Try to import test cases, ignore modules on fail
 try:
     from submodules.antenna_deploy.tests import run_tests as run_antenna_tests
+    MODULES["antenna"] = run_antenna_tests
 except:
     MODULES["antenna"] = False
 try:
     from submodules.command_ingest.tests import run_tests as run_command_tests
+    MODULES["command"] = run_command_tests
 except:
     MODULES["command"] = False
 try:
     from submodules.eps.tests import run_tests as run_eps_tests
+    MODULES["eps"] = run_eps_tests
 except:
     MODULES["eps"] = False
 try:
     from submodules.radios.aprs_test import run_tests as run_aprs_tests
+    MODULES["radio.aprs"] = run_aprs_tests
     APRS_PORT = "FAKE"
 except:
     MODULES["radios.aprs"] = False
 try:
     from submodules.radios.iridium_tests import run_tests as run_iridium_tests
+    MODULES["radios.iridium"] = run_iridium_tests
 except:
     MODULES["radios.iridium"] = False
 try:
     from submodules.telemetry.tests import run_tests as run_telemetry_tests
+    MODULES["telemetry"] = run_telemetry_tests
 except:
     MODULES["telemetry"] = False
 
 if __name__ == '__main__':
-    if MODULES["antenna"]:
-        run_antenna_tests()
-    if MODULES["command"]:
-        run_command_tests()
-    if MODULES["eps"]:
-        run_eps_tests()
-    if MODULES["radios.aprs"]:
-        run_aprs_tests(APRS_PORT)
-    if MODULES["radios.iridium"]:
-        run_iridium_tests()
-    if MODULES["telemetry"]:
-        run_telemetry_tests()
+    for module, func in MODULES, func:
+        if func:
+            func()
