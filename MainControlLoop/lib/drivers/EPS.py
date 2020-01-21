548f89066f13228e6c6fec066ef60b6ca44a6dc4
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2020-01-21 17:18:52 -0500

    write and wait for response methods

--- MainControlLoop/lib/drivers/EPS.py
+++ MainControlLoop/lib/drivers/EPS.py
@@ -14,6 +14,14 @@ class EPS(Device):
     def is_open(self):
         return self.bus.fd is not None
 
+    def write_i2c_block_response(self, register, data):
+        self.write_i2c_block_data(register, data)
+        return self.read_byte()
+
+    def write_byte_response(self, register, value):
+        self.write_byte_data(register, value)
+        return self.read_byte()
+
     def read_byte(self):
         self.bus.open(self.bus_name)
         next_byte = self.bus.read_byte(self.address)
