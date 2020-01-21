ad39f8490cf527fccf80d6b2af5144f02b32bef9
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2020-01-21 17:14:56 -0500

    SMBus wrapper methods

--- MainControlLoop/lib/drivers/EPS.py
+++ MainControlLoop/lib/drivers/EPS.py
@@ -11,6 +11,25 @@ class EPS(Device):
         self.bus_name = '/dev/i2c-1'
         self.address = 0x57
 
+    def is_open(self):
+        return self.bus.fd is not None
+
+    def read_byte(self):
+        self.bus.open(self.bus_name)
+        next_byte = self.bus.read_byte(self.address)
+        self.bus.close()
+        return next_byte
+
+    def write_i2c_block_data(self, register, data):
+        self.bus.open(self.bus_name)
+        self.bus.write_i2c_block_data(self.address, register, data)
+        self.bus.close()
+
+    def write_byte_data(self, register, value):
+        self.bus.open(self.bus_name)
+        self.bus.write_byte_data(self.address, register, value)
+        self.bus.close()
+
     def functional(self):
         raise NotImplementedError
 
