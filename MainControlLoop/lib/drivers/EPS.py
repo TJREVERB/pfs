cdeb5d085b9073228d65be47ec81d3b1c0a9c8c8
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2020-01-21 21:26:48 -0500

    limited i2c register

--- MainControlLoop/lib/drivers/EPS.py
+++ MainControlLoop/lib/drivers/EPS.py
@@ -1,6 +1,13 @@
+from enum import Enum
+
 from MainControlLoop.lib.devices import Device
 
 from smbus2 import SMBus
+
+
+class EPSRegister(Enum):
+    # TODO: Populate with actual register values
+    PDM_STATUS = 0x0E
 
 
 class EPS(Device):
@@ -14,13 +21,28 @@ class EPS(Device):
     def is_open(self):
         return self.bus.fd is not None
 
-    def write_i2c_block_response(self, register, data):
+    def write_i2c_block_response(self, register: EPSRegister, data):
+        if type(register) != EPSRegister:
+            return
+
         self.write_i2c_block_data(register, data)
         return self.read_byte()
 
-    def write_byte_response(self, register, value):
+    def write_byte_response(self, register: EPSRegister, value):
+        if type(register) != EPSRegister:
+            return
+
         self.write_byte_data(register, value)
         return self.read_byte()
+
+    def read_byte_data(self, register: EPSRegister):
+        if type(register) != EPSRegister:
+            return
+
+        self.bus.open(self.bus_name)
+        next_byte = self.bus.read_byte_data(self.address, register.value)
+        self.bus.close()
+        return next_byte
 
     def read_byte(self):
         self.bus.open(self.bus_name)
@@ -28,14 +50,20 @@ class EPS(Device):
         self.bus.close()
         return next_byte
 
-    def write_i2c_block_data(self, register, data):
+    def write_i2c_block_data(self, register: EPSRegister, data):
+        if type(register) != EPSRegister:
+            return
+
         self.bus.open(self.bus_name)
-        self.bus.write_i2c_block_data(self.address, register, data)
+        self.bus.write_i2c_block_data(self.address, register.value, data)
         self.bus.close()
 
-    def write_byte_data(self, register, value):
+    def write_byte_data(self, register: EPSRegister, value):
+        if type(register) != EPSRegister:
+            return
+
         self.bus.open(self.bus_name)
-        self.bus.write_byte_data(self.address, register, value)
+        self.bus.write_byte_data(self.address, register.value, value)
         self.bus.close()
 
     def functional(self):
