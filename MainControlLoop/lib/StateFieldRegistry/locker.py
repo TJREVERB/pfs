7b7beaeceb320f5408c76820ed6f083b071d3dd6
Ron <gokepler@gmail.com>
2020-02-06 17:30:21 -0500

    Finished SFR Locker

--- MainControlLoop/lib/StateFieldRegistry/locker.py
+++ MainControlLoop/lib/StateFieldRegistry/locker.py
@@ -9,42 +9,28 @@ class StateFieldRegistryLocker:
 
    def store(self, state_field_registry):
       """
-      Adds a StateFieldRegistry to the locker as a tuple: (Timestamp, Corresponding Dump)
+      Adds a StateFieldRegistry to the locker as a tuple: (Timestamp, Dump)
       Preserves ordering by timestamp (least to greatest, oldest to newest)
       :param state_field_registry: StateFieldRegistry to add to Locker
       """
-
       sfrTuple = (state_field_registry.get(StateField.SYS_TIME), DownLinkProducer.create_dump(state_field_registry))    # Creates a tuple with sfrTuple[0] = timestamp, sfrTuple[1] = dump of SFR
-      if len(self.locker) == 0:
-         self.locker.append(sfrTuple)
-      else:
-         index = len(self.locker)
-         for i in range(len(self.locker)):
-            if self.locker[i][0] >= sfrTuple[0]:
-               index = i
-               break
-         self.locker.insert(index, sfrTuple)
+      index = len(self.locker)
+      for i in range(len(self.locker)):
+         if self.locker[i][0] > sfrTuple[0]:
+            index = i
+            break
+      self.locker = self.locker[:index] + [sfrTuple] + self.locker[index:]
 
    def find(self, timeReq):
       """
       Finds the StateFieldRegistry stored in the locker with timestamp closest to 'timeReq'
       :param timeReq: Timestamp requested
       """
-
-      if len(self.locker) > 0:
-         for i in range(len(self.locker)):
-            if timeReq == self.locker[i][0]:
-               return self.locker[i][1]
-            elif timeReq >= self.locker[i][0]:
-               downDiff = timeReq - self.locker[i][0]
-               upDiff = self.locker[i+1][0] - timeReq
-               if downDiff < upDiff:
-                  return self.locker[i][1]
-               else:
-                  return self.locker[i+1][1]
-         return self.locker[-1][1]
-      else:
+      if len(self.locker) == 0:
          return None
+      else:
+         return min(range(len(self.locker)), key=lambda i: abs(self.locker[i][0]-timeReq))
+
 
 locker = StateFieldRegistryLocker()
 state_field_registry = StateFieldRegistry()
