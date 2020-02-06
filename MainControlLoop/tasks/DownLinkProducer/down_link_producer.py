cf64494bc145b27b353a5a81d1b076f1d9fa6cad
Ethan Nguyen <etnguyen03@hotmail.com>
2020-02-06 16:34:25 -0500

    Add dummy downlinkproducer

---
+++ MainControlLoop/tasks/DownLinkProducer/down_link_producer.py
@@ -0,0 +1,18 @@
+class DownLinkProducer:
+    @staticmethod
+    def create_dump(state_field_registry) -> str:
+        """
+        Creates a dump from the given state_field_registry.
+        :param state_field_registry: A state field registry.
+        :return: A formatted dump.
+        """
+        return ""
+
+    @staticmethod
+    def create_beacon(state_field_registry) -> str:
+        """
+
+        :param state_field_registry:
+        :return:
+        """
+        return ""
