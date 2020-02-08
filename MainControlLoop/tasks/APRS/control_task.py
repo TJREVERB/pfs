70531722fac621c61e8398e81f2a16758007160c
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2020-02-08 16:13:14 -0500

    implemented DownLinkProduer

--- MainControlLoop/tasks/APRS/control_task.py
+++ MainControlLoop/tasks/APRS/control_task.py
@@ -4,15 +4,16 @@ from MainControlLoop.lib.modes import Mode
 from MainControlLoop.tasks.APRS.beacon_actuate_task import APRSBeaconActuateTask
 from MainControlLoop.tasks.APRS.dump_actuate_task import APRSDumpActuateTask
 from MainControlLoop.tasks.APRS.crticial_message_actuate_task import APRSCriticalMessageActuateTask
+from MainControlLoop.tasks.DownLinkProducer import DownLinkProducer
 
 from enum import Enum
 
 
 class BeaconInterval(Enum):
-    FAST = "FAST"
-    SLOW = "SLOW"
-    CUSTOM = "CUSTOM"
-    NEVER = "OFF"
+    FAST = 30
+    SLOW = 120
+    CUSTOM = -1
+    NEVER = 1
 
 
 class APRSControlTask:
@@ -20,14 +21,6 @@ class APRSControlTask:
     def __init__(self, aprs: APRS, state_field_registry: StateFieldRegistry, mode: Mode, beacon_actuate_task: APRSBeaconActuateTask, dump_actuate_task: APRSDumpActuateTask, critical_message_actuate_task: APRSCriticalMessageActuateTask):
         self.aprs: APRS = aprs
         self.state_field_registry: StateFieldRegistry = state_field_registry
-
-        self.beacon_interval_lookup: dict = {
-            BeaconInterval.FAST: 30,
-            BeaconInterval.SLOW: 120,
-            BeaconInterval.CUSTOM: -1,
-            BeaconInterval.NEVER: -1,
-        }
-        self.beacon_interval: BeaconInterval = BeaconInterval.NEVER
         self.mode: Mode = mode
 
         self.beacon_actuate_task: APRSBeaconActuateTask = beacon_actuate_task
@@ -39,27 +32,27 @@ class APRSControlTask:
         # TODO: control logic HAS NOT been written for parsing commands
 
         if self.mode == Mode.SAFE:
-            self.beacon_interval = BeaconInterval.NEVER
-            # TODO: create a reset actuate task
+            self.state_field_registry.update(StateField.APRS_BEACON_INTERVAL, BeaconInterval.NEVER.value)
+            # TODO: decide safe mode logic
             return
 
         if self.mode == Mode.LOW_POWER:
-            self.beacon_interval = BeaconInterval.SLOW
+            self.state_field_registry.update(StateField.APRS_BEACON_INTERVAL, BeaconInterval.SLOW.value)
 
         if self.mode == Mode.COMMS:
-            # TODO: down link producer needs to create a dump to send down
-            self.dump_actuate_task.set_dump("dump")
+            # TODO: use the SFR Locker
+            self.state_field_registry.update(StateField.APRS_BEACON_INTERVAL, BeaconInterval.NEVER.value)
+            self.dump_actuate_task.set_dump(DownLinkProducer.create_dump(self.state_field_registry))
             self.dump_actuate_task.run = True
-            self.beacon_interval = BeaconInterval.NEVER
 
         if self.mode == Mode.NORMAL:
-            self.beacon_interval = BeaconInterval.Fast
+            self.state_field_registry.update(StateField.APRS_BEACON_INTERVAL, BeaconInterval.FAST.value)
 
-        # TODO: figure out if beacon interval should be stored in SFR or in Control Task
+        interval = self.state_field_registry.get(StateField.APRS_BEACON_INTERVAL)
         last_beacon_time: float = self.state_field_registry.get(StateField.APRS_LAST_BEACON_TIME)
         current_sys_time: float = self.state_field_registry.get(StateField.TIME)
 
-        if current_sys_time - last_beacon_time > self.beacon_interval_lookup[self.beacon_interval]:
-            # TODO: down link producer needs to create a beacon to send down
-            self.beacon_actuate_task.set_beacon("beacon")
+        if current_sys_time - last_beacon_time > interval:
+            beacon = DownLinkProducer.create_beacon(self.state_field_registry)
+            self.beacon_actuate_task.set_beacon(beacon)
             self.beacon_actuate_task.run = True
