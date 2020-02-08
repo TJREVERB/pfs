4c04d9dc7ee0a8dfcb1fdb3e5ed412382922e183
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2020-02-08 16:14:26 -0500

    formatting

--- MainControlLoop/main_control_loop.py
+++ MainControlLoop/main_control_loop.py
@@ -10,19 +10,19 @@ class MainControlLoop:
     def __init__(self):
         self.state_field_registry: StateFieldRegistry = StateFieldRegistry()
         self.pi_monitor: PiMonitorTask = PiMonitorTask(self.state_field_registry)
-        self.APRS_task: APRSTask = APRSTask(self.state_field_registry)
+        self.aprs_task: APRSTask = APRSTask(self.state_field_registry)
         self.iridium_task: IridiumTask = IridiumTask(self.state_field_registry)
-        self.core: Core = Core(self.state_field_registry, self.APRS_task)
+        self.core: Core = Core(self.state_field_registry, self.aprs_task)
 
     def execute(self):
         # READ BLOCK
         self.pi_monitor.read()
-        APRS_message = self.APRS_task.read()
+        aprs_message = self.aprs_task.read()
         iridium_message = self.iridium_task.read()
 
         # CONTROL BLOCK
-        self.core.control([APRS_message, iridium_message])
-        self.APRS_task.control([APRS_message, iridium_message])
+        self.core.control([aprs_message, iridium_message])
+        self.aprs_task.control([aprs_message, iridium_message])
 
         # ACTUATE BLOCK
-        self.APRS_task.actuate()
+        self.aprs_task.actuate()
