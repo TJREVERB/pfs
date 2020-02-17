411e640013aa53cc5ad451086875f0c01353f115
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2020-02-17 00:22:16 -0500

    fixed APRS read task

--- MainControlLoop/tasks/APRS/read_task.py
+++ MainControlLoop/tasks/APRS/read_task.py
@@ -19,7 +19,7 @@ class APRSReadTask:
         if current_time - last_message_time > self.CLEAR_BUFFER_TIMEOUT:
             self.buffer = []
 
-        next_byte, success = self.aprs.read()
+        next_bytes, success = self.aprs.read()
 
         if success is False:
             # APRS Hardware Fault
@@ -28,17 +28,17 @@ class APRSReadTask:
 
         self.state_field_registry.drop_flag(ErrorFlag.APRS_FAILURE)
 
-        if len(next_byte) == 0:
+        if len(next_bytes) == 0:
             return
 
-        if next_byte == '\n'.encode('utf-8'):
+        self.buffer.append(next_bytes)
+
+        if '\n'.encode('utf-8') in next_bytes:
             message: str = ""
             while len(self.buffer) > 0:
                 buffer_byte: bytes = self.buffer.pop(0)
                 message += buffer_byte.decode('utf-8')
 
-            self.last_message = message
+            self.last_message = message.replace('\n', '')
             self.state_field_registry.update(StateField.APRS_LAST_MESSAGE_TIME, current_time)
             return
-
-        self.buffer.append(next_byte)
