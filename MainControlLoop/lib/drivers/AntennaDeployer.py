dc871da3478d8bd92e9bcfbc44695ba6cc0461e3
Srikar Gouru <srikarg89@gmail.com>
2020-02-27 18:26:25 -0500

    AntennaDeployer driver

--- MainControlLoop/lib/drivers/AntennaDeployer.py
+++ MainControlLoop/lib/drivers/AntennaDeployer.py
@@ -1,79 +1,69 @@
+import time
+from enum import Enum
 from smbus2 import SMBus
-
 from MainControlLoop.lib.devices import Device
 
-from enum import Enum
 
-
-class AntennaDeployerRegister(Enum):
-    RESET = 0xAA
-    ARM = 0xAD
-    DISARM = 0xAC
-    DEPLOY_ANTENNA_1 = 0xA1
-    DEPLOY_ANTENNA_2 = 0xA2
-    DEPLOY_ANTENNA_3 = 0xA3
-    DEPLOY_ANTENNA_4 = 0xA4  # TODO: documentation says deploy methods need a parameter, figure out if this is the register parameter for i2c call
+class AntennaDeployerCommand(Enum):
+    SYSTEM_RESET = 0xAA
+    WATCHDOG_RESET= 0xCC
+    ARM_ANTS=0xAD
+    DISARM_ANTS= 0xAC
+    DEPLOY_1=0xA1
+    DEPLOY_2=0xA2
+    DEPLOY_3=0xA3
+    DEPLOY_4=0xA4
+    AUTO_DEPLOY= 0xA5
+    DEPLOY_1_OVERRIDE=0xBA
+    DEPLOY_2_OVERRIDE=0xBB
+    DEPLOY_3_OVERRIDE=0xBC
+    DEPLOY_4_OVERRIDE=0xBD
+    CANCEL_DEPLOY=0xA9
+    GET_TEMP=0xC0
+    GET_STATUS=  0xC3
+    GET_UPTIME_SYS=   0xC6
+    GET_TELEMETRY=0xC7
+    GET_COUNT_1= 0xB0
+    GET_COUNT_2= 0xB1
+    GET_COUNT_3= 0xB2
+    GET_COUNT_4= 0xB3
+    GET_UPTIME_1=0xB4
+    GET_UPTIME_2=0xB5
+    GET_UPTIME_3=0xB6
+    GET_UPTIME_4=0xB7
 
 
 class AntennaDeployer(Device):
-    BUS_NAME = '/dev/i2c-2'
-    ADDRESS = 0x58
 
     def __init__(self):
         super().__init__("antenna_deployer")
-        self.bus = SMBus()
+        self.bus = SMBus(1)
+        self.primary_address = 0x31
+        self.secondary_address = 0x32
+        self.timeout = 10
 
-    def write_i2c_block_response(self, register: AntennaDeployerRegister, data) -> bytes or None:
-        if type(register) != AntennaDeployerRegister:
-            return
+    def write_i2c_word_data(self, command: AntennaDeployerCommand, data: int) -> bool:
+        if type(command) != AntennaDeployerCommand:
+            return False
 
-        self.write_i2c_block_data(register, data)
-        return self.read_byte()
+        self.bus.write_i2c_word_data(self.primary_address, command.value, data)
+        return True
 
-    def write_byte_response(self, register: AntennaDeployerRegister, value) -> bytes or None:
-        if type(register) != AntennaDeployerRegister:
-            return
+    def read_i2c_word_data(self, command: AntennaDeployerCommand) -> bytes or None:
+        if type(command) != AntennaDeployerCommand:
+            return None
 
-        self.write_byte_data(register, value)
-        return self.read_byte()
+        bus.write_word_data(self.primary_address, command.value, 0x00)
+        time.sleep(0.5)
+        return bus.read_byte(self.primary_address)
 
-    def read_byte_data(self, register: AntennaDeployerRegister) -> bytes or None:
-        if type(register) != AntennaDeployerRegister:
-            return
-
-        self.bus.open(self.BUS_NAME)
-        next_byte = self.bus.read_byte_data(self.ADDRESS, register.value)
-        self.bus.close()
-        return next_byte
-
-    def read_byte(self) -> bytes or None:
-        self.bus.open(self.BUS_NAME)
-        next_byte = self.bus.read_byte(self.ADDRESS)
-        self.bus.close()
-        return next_byte
-
-    def write_i2c_block_data(self, register: AntennaDeployerRegister, data):
-        if type(register) != AntennaDeployerRegister:
-            return
-
-        self.bus.open(self.BUS_NAME)
-        self.bus.write_i2c_block_data(self.ADDRESS, register.value, data)
-        self.bus.close()
-
-    def write_byte_data(self, register: AntennaDeployerRegister, data):
-        if type(register) != AntennaDeployerRegister:
-            return
-
-        self.bus.open(self.BUS_NAME)
-        self.bus.write_byte_data(self.ADDRESS, register.value, data)
-        self.bus.close()
 
     def deploy(self, register: AntennaDeployerRegister):
-        self.write_byte_data(register, register.ARM)
-        self.write_byte_data(register, register.DEPLOY_ANTENNA_1)
-        self.write_byte_data(register, register.DEPLOY_ANTENNA_2)
-        self.write_byte_data(register, register.DEPLOY_ANTENNA_3)
-        self.write_byte_data(register, register.DEPLOY_ANTENNA_4)
+        self.write_i2c_word_data(AntennaDeployerCommand.ARM_ANTS)
+        self.write_i2c_word_data(AntennaDeployerCommand.DEPLOY_1)
+        self.write_i2c_word_data(AntennaDeployerCommand.DEPLOY_2)
+        self.write_i2c_word_data(AntennaDeployerCommand.DEPLOY_3)
+        self.write_i2c_word_data(AntennaDeployerCommand.DEPLOY_4)
 
     def functional(self):
         """
@@ -90,7 +80,7 @@ class AntennaDeployer(Device):
             self.bus.open(self.BUS_NAME)
         except:
             return False
-        self.bus.write_byte(self.ADDRESS, AntennaDeployerRegister.RESET)
+        self.write_i2c_word_data(AntennaDeployerCommand.SYSTEM_RESET)
 
     def disable(self):
         """
@@ -100,7 +90,7 @@ class AntennaDeployer(Device):
             self.bus.open(self.BUS_NAME)
         except:
             return False
-        self.bus.write_byte(self.ADDRESS, AntennaDeployerRegister.DISARM)
+        self.write_i2c_word_data(AntennaDeployerCommand.DISARM_ANTS)
 
     def enable(self):
         """
@@ -110,4 +100,4 @@ class AntennaDeployer(Device):
             self.bus.open(self.BUS_NAME)
         except:
             return False
-        self.bus.write_byte(self.ADDRESS, AntennaDeployerRegister.ARM.value)
+        self.write_i2c_word_data(AntennaDeployerCommand.ARM_ANTS)
